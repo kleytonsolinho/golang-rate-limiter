dc81cced59eedf09347702c52756c35ee33b5073
kleytonsolinho <kleytonsolinho@gmail.com>
2024-06-10 11:46:18 -0300

    chore: Implement rate limiting middleware for server with IP and token-based limits

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,27 +12,64 @@ import (
 )
 
 func main() {
-	config, _ := configs.LoadConfig(".")
+	config, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
 	rateLimitPerSecond, _ := strconv.Atoi(config.RateLimitPerSecond)
+	rateLimitWithTokenPerSecond, _ := strconv.Atoi(config.RateLimitWithTokenPerSecond)
 
-	r.Use(httprate.Limit(
-		rateLimitPerSecond, // requests
+	limitByIP := httprate.Limit(
+		rateLimitPerSecond, // requests per IP
 		1*time.Second,      // per duration
 		httprate.WithLimitHandler(func(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "you have reached the maximum number of requests or actions allowed within a certain time frame", http.StatusTooManyRequests)
 		}),
 		httprate.WithKeyFuncs(func(r *http.Request) (string, error) {
-			return r.Header.Get("X-Access-Token"), nil
+			token := r.Header.Get("X-Ratelimit-Token")
+			if token == "" {
+				return r.RemoteAddr, nil
+			}
+			return "", nil
 		}),
-	))
+	)
+
+	limitByToken := httprate.Limit(
+		rateLimitWithTokenPerSecond, // requests per token
+		1*time.Second,               // per duration
+		httprate.WithLimitHandler(func(w http.ResponseWriter, r *http.Request) {
+			http.Error(w, "you have reached the maximum number of requests or actions allowed within a certain time frame", http.StatusTooManyRequests)
+		}),
+		httprate.WithKeyFuncs(
+			func(r *http.Request) (string, error) {
+				token := r.Header.Get("X-Ratelimit-Token")
+				if token != "" {
+					return token, nil
+				}
+				return "", nil
+			},
+		),
+	)
+
+	r.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			token := r.Header.Get("X-Ratelimit-Token")
+
+			if token == "" {
+				limitByIP(next).ServeHTTP(w, r)
+			} else {
+				limitByToken(next).ServeHTTP(w, r)
+			}
+		})
+	})
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Success!"))
 	})
 
-	http.ListenAndServe(config.WebServerPort, r)
+	http.ListenAndServe(":"+config.WebServerPort, r)
 }
